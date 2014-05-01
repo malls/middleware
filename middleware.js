227964a4de9ee95e48e18572d2c4566ab32b8300
Forrest Almasi <forrestalmasi@gmail.com>
2014-05-01 17:12:44 -0400

    Initial setup

---
+++ middleware.js
@@ -0,0 +1,13 @@
+// make these functions
+
+  // - createServer()
+  // - use()
+  // - listen()
+
+var createServer = function(){};
+var use = function(){};
+var listen = function(){};
+
+module.exports.createServer = createServer;
+module.exports.use = use;
+module.exports.listen = listen;
